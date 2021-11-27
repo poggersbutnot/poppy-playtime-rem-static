d440e5b5315f6dc81b9f6c2bebef54cde9b1b4ea
Karim <77207877+poggersbutnot@users.noreply.github.com>
2021-11-27 16:52:52 +0400

    Update lastModifiedScript.js

--- logs/lastModifiedScript.js
+++ logs/lastModifiedScript.js
@@ -1,26 +1,24 @@
 var camname = document.querySelector("#camera-name").textContent.replaceAll(/[A-Z]/ig,'').replaceAll(" ",'').replaceAll('<', '').replaceAll('>', '')
 
 // https://stackoverflow.com/a/2314027/
-function fetchHeader(url, wch) {
+function fetchHeader(a, b) {
     try {
-        var req=new XMLHttpRequest();
-        req.open("HEAD", url, false);
-        req.send(null);
-        if(req.status== 200){
-            return req.getResponseHeader(wch);
-        }
-        else return false;
-    } catch(er) {
-        return er.message;
+        var c = new XMLHttpRequest();
+        return (
+            c.open("HEAD", a, !1),
+            c.send(null),
+            200 == c.status && c.getResponseHeader(b)
+        );
+    } catch (a) {
+        return a.message;
     }
 }
 
-if(camname === '01') {
-   var url = 'https://poppyplaytimeco.com/assets/hub_cam01.gif'
-} else if (camname === '08') {
-   url = 'https://poppyplaytimeco.com/assets/machine_room_cam01.gif'
-} else if (camname === '27') {
-   url = 'https://poppyplaytimeco.com/assets/catwalk_cam01.gif'
-}
-
-alert(fetchHeader(url,'Last-Modified'));
+if ("01" === camname)
+    var url = "https://poppyplaytimeco.com/assets/hub_cam01.gif";
+else
+    "08" === camname
+        ? (url = "https://poppyplaytimeco.com/assets/machine_room_cam01.gif")
+    : "27" === camname &&
+        (url = "https://poppyplaytimeco.com/assets/catwalk_cam01.gif");
+alert(fetchHeader(url, "Last-Modified"));
